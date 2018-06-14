c4c9ac76513b5cf6032eadade79dca5f9dd1d88b
Manuel Osvaldo Olguín <molguin@kth.se>
2018-06-14 11:19:56 +0200

    db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI',
+                                                  'sqlite:////tmp/test.db')
+db = SQLAlchemy(app)
 
 
 @app.route('/')
