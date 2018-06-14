a6f2d8fdef2b399e2f4c1f852cb02fd595ff5abb
Manuel Osvaldo Olguín <molguin@kth.se>
2018-06-14 15:03:14 +0200

    begin work on models

--- app/models/dance.py
+++ app/models/dance.py
@@ -1,4 +1,20 @@
 from app import db
 
-class Turn:
+
+class Dance(db.Model):
+    __tablename__ = 'dance'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    turns = db.relationship('Turn', backref='dance')
+
+
+class Turn(db.Model):
+    __tablename__ = 'turn'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    dance_id = db.Column(db.Integer, db.ForeignKey('dance.id'), nullable=False)
+
     pass
